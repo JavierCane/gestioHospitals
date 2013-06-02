076d125a57410015db00860f5b583eea7399c3d6
aperez77 <albert.perez.huertas@gmail.com>
2013-06-02 23:03:03 +0200

    Update Hospital.java

--- src/gestiohospitals/domini/models/Hospital.java
+++ src/gestiohospitals/domini/models/Hospital.java
@@ -1,115 +1,60 @@
+
 package gestiohospitals.domini.models;
 
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table( name = "hospital" )
-public class Hospital implements Serializable {
+@Table(name="hospital")
+public class Hospital {
+    @Id
+    @GeneratedValue
+    @Column(name="nom")
+    private String nom;
+    @Column(name="adreca")
+    private String adreca;
+    @Column(name="descripcio")
+    private String descripcio;
+    @OneToMany(mappedBy="hospital")
+    private Set<Ingres> ingresos;
 
-	@Id
-        @Column(name="nom")
-	private String nom;
-	@Column(name="adreca")
-	private String adreca;
-	@Column(name="descripcio")
-	private String descripcio;
-	
-	@OneToMany( mappedBy = "id.hospital", cascade = CascadeType.ALL, fetch = FetchType.LAZY )
-	private Set<Habitacio> habitacio = new HashSet<>();
+    public Hospital() {
+    }
+    
+    public Hospital(String nom, String adreca, String descripcio) {
+        this.nom = nom;
+        this.adreca = adreca;
+        this.descripcio = descripcio;
+    }
 
-	public Hospital() {
-	}
+    public String getNom() {
+        return nom;
+    }
 
-	public Hospital( String nom, String adreca, String descripcio ) {
-		this.nom = nom;
-		this.adreca = adreca;
-		this.descripcio = descripcio;
-	}
-	
-	@Override
-	public String toString() {
-		if ( habitacio == null )
-		{
-			return "Hospital [nom=" + nom + ", adreca=" + adreca + ", descripcio="
-					+ descripcio + "]";
-		}
-		else
-		{
-			return "Hospital [nom=" + nom + ", adreca=" + adreca + ", descripcio="
-				+ descripcio + ", habitacio=" + habitacio.toString() + "]";
-		}
-	}
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-	@Override
-	public int hashCode() {
-		int hash = 7;
-		hash = 67 * hash + Objects.hashCode(this.nom);
-		hash = 67 * hash + Objects.hashCode(this.adreca);
-		hash = 67 * hash + Objects.hashCode(this.descripcio);
-		hash = 67 * hash + Objects.hashCode(this.habitacio);
-		return hash;
-	}
+    public String getAdreca() {
+        return adreca;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final Hospital other = (Hospital) obj;
-		if (!Objects.equals(this.nom, other.nom)) {
-			return false;
-		}
-		return true;
-	}
+    public void setAdreca(String adreca) {
+        this.adreca = adreca;
+    }
 
-	public String getId() {
-		return nom;
-	}
-	
+    public String getDescripcio() {
+        return descripcio;
+    }
 
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getAdreca() {
-		return adreca;
-	}
-
-	public void setAdreca(String adreca) {
-		this.adreca = adreca;
-	}
-
-	public String getDescripcio() {
-		return descripcio;
-	}
-
-	public void setDescripcio(String descripcio) {
-		this.descripcio = descripcio;
-	}
-
-	public Set<Habitacio> getHabitacio() {
-		return habitacio;
-	}
-
-	public void setHabitacio(Set<Habitacio> habitacio) {
-		this.habitacio = habitacio;
-	}
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
+    
+    
 }
-
