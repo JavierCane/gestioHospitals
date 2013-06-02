361940cc3e8fd2351a4962ffa0c94f584e463f13
aperez77 <albert.perez.huertas@gmail.com>
2013-06-02 23:08:47 +0200

    Update Sanitari.java

--- src/gestiohospitals/domini/models/Sanitari.java
+++ src/gestiohospitals/domini/models/Sanitari.java
@@ -1,63 +1,43 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gestiohospitals.domini.models;
 
-import java.io.Serializable;
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="sanitari")
-@Inheritance(strategy=InheritanceType.JOINED)
-//@PrimaryKeyJoinColumn(name="dni")
-@AttributeOverrides({
-    @AttributeOverride(name="nom", column=@Column(name="nom"))
-})
-public class Sanitari extends Persona implements Serializable {
-	
-	@Id
-	@Column(name="codi_empleat")
-	String codi_empleat;
-	@Column(name="nom_hospital")
-	Hospital hospital;
+@Table(name = "sanitari")
+public class Sanitari extends Persona {
+    @Id
+    @GeneratedValue
+    @Column(name="codi_empleat")
+    protected String codiEmpleat;
+    @ManyToOne
+    @JoinColumn(name="nom_hospital")
+    protected Hospital hospital;
 
-    public Sanitari(){
-        super();
-    }
-    
-    public Sanitari(String dni, String nom){
-        super(dni,nom);
-    }
-    public Sanitari(String dni, String nom, String codi_empleat, Hospital hospital){
-        super(dni,nom);
-        this.codi_empleat=codi_empleat;
-        this.hospital=hospital;
-    }
-    
-    public String getCodi_empleat() {
-        return codi_empleat;
+    public Sanitari() {
     }
 
-    public void setCodi_empleat(String codi_empleat) {
-        this.codi_empleat = codi_empleat;
-    }
 
-    public Hospital getHospital() {
-        return hospital;
-    }
 
-    public void setHospital(Hospital hospital) {
+    public Sanitari(String codiEmpleat, Hospital hospital, String dni, String nom) {
+        super(dni, nom);
+        this.codiEmpleat = codiEmpleat;
         this.hospital = hospital;
     }
-        
+
+    public String getCodiEmpleat() {
+        return codiEmpleat;
+    }
+
+    public void setCodiEmpleat(String codiEmpleat) {
+        this.codiEmpleat = codiEmpleat;
+    }
+    
+    
 }
